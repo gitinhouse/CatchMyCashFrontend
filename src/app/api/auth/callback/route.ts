56848a8c742941ba5381416e1b675fb90996c162
accesstbilq <accesstbilq@gmail.com>
2026-04-02 13:48:08 +0530

    Update Callback

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -18,7 +18,7 @@ export async function GET(req: Request) {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI,
+    'https://catchmycash.com/api/auth/callback',
   );
 
   const { tokens } = await oauth2Client.getToken(code);
--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI,
+    'https://catchmycash.com/api/auth/callback',
   );
 
   const state = crypto.randomBytes(32).toString('hex');
